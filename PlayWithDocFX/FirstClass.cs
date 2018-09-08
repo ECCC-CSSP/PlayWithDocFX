c7bc3c1940b38d0a65c43c732808a52e673855fa
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-09-08 14:36:47 -0300

    Initial commit

---
+++ PlayWithDocFX/FirstClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayWithDocFX
+{
+    public partial class FirstClass
+    {
+        #region Variables
+        public string FirstVariable = "bonjour";
+        private string FirstPrivateVariable = "testing";
+        #endregion Variables
+
+        #region Properties
+        public string FirstProperty { get; set; }
+        private string FirstPrivateProperty { get; set; }
+        public SecondClass secondClass { get; set; }
+        #endregion Properties
+
+        #region Constructors
+        public FirstClass()
+        {
+            secondClass = new SecondClass();
+        }
+        #endregion Constructors
+
+        #region Functions public
+        public string GetFirstVariable()
+        {
+            return FirstVariable;
+        }
+        #endregion Functions public
+
+        #region Functions private
+        private string GetFirstPrivateVariable()
+        {
+            return FirstPrivateVariable;
+        }
+        #endregion Function private
+
+
+    }
+}
